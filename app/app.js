bbbe8cd78f4c5ada2d1b187f057792594affe12a
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2023-08-10 21:31:01 -0300

    Accordion

---
+++ app/app.js
@@ -0,0 +1,33 @@
+var accdr = document.getElementsByClassName("accordion");
+
+var closebtn = document.getElementsByClassName("close_btn");
+
+var i;
+
+for (i = 0; i < accdr.length; i++) {
+  /* Accionar boton abrir - escudos */
+
+  accdr[i].addEventListener("click", function () {
+    
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = 75 + "vh";
+    } 
+  });
+
+  /* Accionar boton cerrar */
+  closebtn[i].addEventListener("click", function () {
+    var parent = this.parentNode;
+     if (parent.style.maxHeight) {
+      parent.style.maxHeight = null;
+    } else {
+      parent.style.maxHeight = 1 + "vh";
+    }
+  });
+}
+
+
+
